8a28d2bce4b32a9222d197a3ba5a0729c5f9507e
Gil <frogvsdogvsmarsmelo@gmail.com>
2022-07-28 16:15:42 +0300

    Changed database

--- src/Components/MyAccount.js
+++ src/Components/MyAccount.js
@@ -8,7 +8,7 @@ function MyAccount() {
             <div className='main-div'>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIIpad4HAv80fHFhOyWc5B8_wpeklYpmVtmVzxVrctIw3bRdoTbvv86VHZqTHZA-SWvpE&usqp=CAU' alt='avatar'></img>
                 <h1>Hello Ronen</h1>
-                <div>
+                <div className=''>
                     <button className='btn btn-outline-warning' >Edit My Profile</button>
                     <button className='btn btn-outline-danger' >My WishList</button>
                 </div>
